a82448b7f505179877e0c286261220027e3c00cf
fardeen857 <20me0037@vvce.ac.in>
2021-03-29 17:55:43 +0000

    4c

--- Program-4.c
+++ Program-4.c
@@ -17,7 +17,7 @@ int main()
     struct math avg;
     printf("Enter a number1,number2,number3:\n");
     scanf("%f%f%f", &enternum.num1,&enternum.num2,&enternum.num3);
-    avg.average=(enternum.num1+enternum.num2+enternum.num3)/3
+    avg.average=(enternum.num1+enternum.num2+enternum.num3)/3;
     printf("average=%f",avg.average);
      //compute and print the average
     return 0;
